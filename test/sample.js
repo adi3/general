ebc7cdd64a0784186fd028db68cef80545d5a8cd
Adi Singh <1301549+adi3@users.noreply.github.com>
2023-07-10 19:30:33 +0200

    Update sample.js

--- test/sample.js
+++ test/sample.js
@@ -1,1 +1,14 @@
 
+function fetch(sub = 'programming') {
+    const axios = require('axios')
+
+    axios.get(`https://www.reddit.com/r/\${sub}.json`)
+    .then((response) => {
+        console.log(response);
+        return response;
+    })
+    .catch((error) => {
+        console.error(error);
+        return null;
+    });
+}
