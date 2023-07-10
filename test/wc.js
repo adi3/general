15487e05cbea38f48b21ff9535cbf5570e7c581a
Adi Singh <1301549+adi3@users.noreply.github.com>
2023-07-10 19:55:42 +0200

    Update wc.js

--- test/wc.js
+++ test/wc.js
@@ -1,46 +1,64 @@
-/**
-* @description This function is a promise that is used to fetch data
-* from a server. It takes in the HTTP method, the URL to
-* fetch data from, the body of the request, and the options for
-* the request. It then sets up a new promise that will resolve
-* when the data is received from the server. The promise is then
-* passed to the setMehod function, which is responsible for making the HTTP
-* request.
-* 
-* @param { string } method - The `method` input parameter in this
-* function is used to specify the HTTP method to use when making
-* the request. It can be one of the common HTTP methods such
-* as `GET`, `POST`, `PUT`, etc.
-* 
-* @param { string } path - The `path` input parameter in this
-* function is used to specify the HTTP method to use for the
-* request.
-* 
-* 
-* 
-* @param { object } body - The `body` input parameter is used
-* to specify the HTTP method and the data payload that should be
-* sent to the server. In the case of the `_fetch` function, it
-* is used to specify the HTTP method and the data payload that
-* should be sent to the server.
-* 
-* @param { object } options - The `options` input parameter in this
-* function is used to pass additional options to the request. These options
-* can be used to modify the request headers, set timeouts, and add
-* additional headers to the request.
-* 
-* @param { any } params - The `params` input parameter is used
-* to pass additional data to the server. In this case, it is
-* used to pass a custom authentication token to the server. The authentication
-* token is sent as a query string parameter in the URL and
-* is used to authenticate the user and retrieve their data.
-* 
-* @returns {  } - The output returned by this function depends
-* on the specific implementation of the _fetch method and the response from
-* the server. In general, it will be an HTTP response object, which
-* can contain information such as the response status code, content type, and
-* headers.
-*/
+import axios from "axios";
+import { toast } from "../components/Toast/Toast";
+import Types from "../redux/_actions/_types";
+import configureStore from "../redux/_store/configureStore";
+
+export { _fetch as fetch };
+let { store } = configureStore();
+
+function logoutUser() {
+  localStorage.removeItem("authorizations");
+
+  store.dispatch({
+    type: Types.RESET_PERSIST_ON_LOGOUT,
+  });
+  window.location.reload();
+}
+
+function handleError(error, reject) {
+  if (!error) {
+    toast.error("Something went wrong, Please login and try again.!!");
+  } else if (error) {
+    const {
+      data: { message },
+    } = error;
+
+    if (message === "Token expired") {
+      setTimeout(() => {
+        logoutUser();
+      }, 1000);
+    }
+    toast.error(message);
+  }
+  reject(error);
+  return;
+}
+
+function handleResponse(successs, resolve) {
+  resolve(successs);
+  return;
+}
+
+function setMehod(method, path, body, options, params) {
+  let config = {};
+  if (options) config.headers = options;
+
+  params = params ? "?" + new URLSearchParams(params).toString() : "";
+  // if (method === "get" || method === "delete") {
+  if (method === "get") {
+    return axios[method](`${path}${params}`, config);
+  } else if (method === "post" || method === "put") {
+    return axios[method](`${path}`, body, config);
+  } else if (method === "delete") {
+    // return axios[method](`${path}`, config);
+
+    return axios.delete(path, {
+      headers: options,
+      data: body,
+    });
+  }
+}
+
 function _fetch(method, path, body, options, params) {
   return new Promise((resolve, reject) => {
     return setMehod(method, path, body, options, params)
