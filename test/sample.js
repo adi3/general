620ab5b4580096b214b2101e51f107ede26f0d79
Adi Singh <1301549+adi3@users.noreply.github.com>
2023-07-11 16:50:10 +0200

    Update sample.js

--- test/sample.js
+++ test/sample.js
@@ -1,16 +1,4 @@
-/**
-* @description This function makes an API request to Reddit's JSON API to
-* retrieve the latest posts in the specified sub-reddit. The response is then
-* logged to the console and returned.
-* 
-* @param { string } [sub='programming'] - The `sub` input parameter in this
-* function is used to specify the name of the subreddit that the
-* function should fetch data from. The `programming` value is hardcoded in the
-* function, but it can be changed to any other subreddit name.
-* 
-* @returns { object } - The output returned by this function is
-* a JSON object containing all the posts from the specified subreddit.
-*/
+
 function fetch(sub = 'programming') {
     const axios = require('axios')
 
