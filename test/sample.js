afd5499e42b520b27c6847d43a0db21b5b85f099
Adi Singh <1301549+adi3@users.noreply.github.com>
2023-08-09 11:08:53 -0700

    Update sample.js

--- test/sample.js
+++ test/sample.js
@@ -1,19 +1,4 @@
 
-/**
-* @description This function fetches data from a Reddit API based on a specified 
-* subreddit. It uses Axios to make a GET request to the API endpoint and logs the 
-* response to the console.
-* 
-* @param { string } [sub='programming'] - The `sub` input parameter in the `fetch` 
-* function is a string that specifies the subreddit to fetch content from. The 
-* function uses the `axios` library to make a GET request to the Reddit API, passing 
-* in the `sub` parameter as part of the URL.
-* 
-* @returns { object } - The output returned by this function is a JSON object 
-* containing the data from the Reddit API. The function uses Axios to make a GET 
-* request to the specified subreddit's JSON feed, and logs the response to the console 
-* before returning it.
-*/
 function fetch(sub = 'programming') {
     const axios = require('axios')
 
