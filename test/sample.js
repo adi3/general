940d86bd2b0fa0a177647b783f3a40e7e7be38b4
Adi Singh <1301549+adi3@users.noreply.github.com>
2023-08-09 11:47:51 -0700

    Update sample.js

--- test/sample.js
+++ test/sample.js
@@ -33,6 +33,7 @@ const search = (arr, x, start, end) => {
 
 
 
+
 const handler = async(event) => {
     try {
         const { sub, email, phone_number: phoneNumber, name, "cognito:username": cognitoUsername, } = event.requestContext.authorizer.jwt.claims;
